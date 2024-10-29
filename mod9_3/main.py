74c3865d095ad2ecf75a8ce3933be1dd409bd1b7
winter <daniillolhigh@mail.ru>
2024-10-29 13:07:28 +0300

    mpd9_3 update

--- mod9_3/main.py
+++ mod9_3/main.py
@@ -2,29 +2,26 @@ x = 8
 y = 10
 while True:
     command = input("Введите команду: ")
-    if command == 'w':
-        if y == 20:
-            print("Вы уперлись в северную стену, ваши координаты: ", x, y)
-        else:
-            y += 1
-            print("Ваши координаты: ", x, y)
-    elif command == 's':
-        if y == 0:
-            print("Вы уперлись в южную стену, ваши координаты: ", x, y)
-        else:
-            y -= 1
-            print("Ваши координаты: ", x, y)
-    elif command == 'a':
-        if x == 0:
-            print("Вы уперлись в западнуб стену, ваши координаты:: ", x, y)
-        else:
-            x -= 1
-            print("Ваши координаты: ", x, y)
-    elif command == 'd':
-        if x == 15:
-            print("Вы уперлись в восточную стену, ваши координаты: ", x, y)
-        else:
-            x += 1
-            print("Ваши координаты: ", x, y)
+    command.lower()
+    if command == 'w' and y == 20:
+        print("Вы уперлись в северную стену, ваши координаты: ", x, y)
+    elif command == 'w' and y < 20:       
+        y += 1
+        print("Ваши координаты: ", x, y)
+    elif command == 's' and y == 0:
+        print("Вы уперлись в южную стену, ваши координаты: ", x, y)
+    elif command == 's' and y > 0:
+        y -= 1
+        print("Ваши координаты: ", x, y)
+    elif command == 'a' and x == 0:
+        print("Вы уперлись в западнуб стену, ваши координаты:: ", x, y)
+    elif command == 'a' and x > 0:
+        x -= 1
+        print("Ваши координаты: ", x, y)
+    elif command == 'd' and x == 15:
+        print("Вы уперлись в восточную стену, ваши координаты: ", x, y)
+    elif command == 'd' and x < 15:
+        x += 1
+        print("Ваши координаты: ", x, y)
     elif command == 'exit':
         break
